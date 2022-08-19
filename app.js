7cfcdb11088598de4ef89e7fd6fddba412b4df6c
Julie Mead <j_me089@yahoo.com>
2022-08-18 22:32:02 -0700

    working demo

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
+const { default: mongoose } = require('mongoose')
 
 
 //Load Config
@@ -18,15 +21,40 @@ connectDB()
 
 const app = express()
 
+//Body Parser
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
+//Method Override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        //look in urlencoded POST bodies and delte it
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
 //Logging
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+//Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+const { application } = require('express')
+
 //Handlebars
 app.engine('.hbs', exphbs.engine({
     defaultLayout: 'main',
-    extname: '.hbs'
+    extname: '.hbs',
+    helpers: {
+        formatDate,
+        stripTags,
+        truncate,
+        editIcon,
+        select
+    }
     }
 ))
 app.set('view engine', '.hbs')
@@ -37,11 +65,22 @@ app.use(
         secret: 'keyboard cat',
         resave: false,
         saveUninitialized: false,
-}))
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URI
+        })
+    })
+)
 
 //Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+//Set global variable
+app.use(function (req,res, next) {
+    res.locals.user = req.user || null
+    next()
+})
+
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
@@ -49,6 +88,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 //Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 8500
 
